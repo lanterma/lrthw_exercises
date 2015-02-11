6e6ef7b3f349b38131d3f5d9cb3fcedb4754450b
Matt Lanterman <Lanterma@gmail.com>
2015-02-10 20:12:03 -0700

    Adding ex3

---
+++ chapter_3/calculate_this.rb
@@ -0,0 +1,6 @@
+# I need to calculate how many beans are in 1 container of hummus
+
+bean_weight = 1.2
+container_size = 32.0
+
+puts "1 container of hummus has #{bean_weight * container_size} beans"
---
+++ chapter_3/ex3.rb
@@ -0,0 +1,37 @@
+# This line writes out 'I will now count my chickens'
+puts "I will now count my chickens:"
+
+# This line writes out 'Hens' and then the answer = '30' according to PEMDAS
+puts "Hens #{25 + 30 / 6}"
+# This line writes out 'Roosters' and then the answer '97'
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# This line writes out 'Now I will count the eggs'
+puts "Now I will count the eggs:"
+
+# This line, does math according to PEMDAS
+# (Parenthesis, Exponents, Multiplication, Division, Add, Subtract)
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# This line writes out 'Is it true that 3 + 2 < 5 - 7' no math at all.
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# This line does math with the answer being False.
+puts 3 + 2 < 5 - 7
+
+# This line asks a question, and then provides the answer which is 5
+puts "What is 3 + 2? #{3 + 2}"
+# This line asks a similar question, and provides the answer which is -2
+puts "what is 5 - 7 #{5 - 7}"
+
+# This line writes out ' Oh, that's why it's false'
+puts "Oh, that's why it's false."
+
+# THis line writes out 'How about some more'
+puts "How about some more."
+
+# These lines use the
+# greater > than, less than <, and greater or equal >= to compare numbers
+puts "Is is greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal #{5 <= -2}"
