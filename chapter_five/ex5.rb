8fe582b576e9f04f154b02077f2dbdd40fa0580d
Matt Lanterman <Lanterma@gmail.com>
2015-02-13 17:14:26 -0700

    Minor change to ex5.rb

--- chapter_five/ex5.rb
+++ chapter_five/ex5.rb
@@ -1,36 +1,27 @@
-# 1 lb = .45 Kg
-# 1 inch = 2.54 Centimeters
 
-#pounds = (gets.chomp).to_i
-#inches = (gets.chomp).to_i
-
-
-
-
-name = 'Matt Lanterman'
-age = 28 # 1987
-height = 76.0  # inches
-weight = 165 # pounds
-eyes = 'Brown'
-teeth = 'white'
-hair = 'Brown'
-
+my_name = 'Matt Lanterman'
+my_age = 28 # 1987
+my_height = 76.0  # inches
+my_weight = 165 # pounds
+my_eyes = 'Brown'
+my_teeth = 'white'
+my_hair = 'Brown'
 
 lb_to_kg_conversion = weight * 0.45
 inch_to_cent_conversion = height * 2.5
 
-puts "Let's talk about #{name}."
-puts "He's #{height} inches tall."
-puts "He's #{weight} pounds heavy."
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{eyes} and #{hair} hair."
-puts "His teeth are usually #{teeth} depending on the coffee."
+puts "He's got #{my_eyes} and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
 
 # I am going to try and get this next, trick line correct.
-puts "If i add #{age}, #{height}, and #{weight}
+puts "If i add #{my_age}, #{my_height}, and #{my_weight}
 I get #{age + height + weight}"
 
 # is this 'working out the math in Ruby?'
-puts "But if we want to convert his weight to Kilograms #{name} would
-weigh #{weight * lb_to_kg_conversion} Kg"
-puts "Also, he would be #{height * inch_to_cent_conversion} centimeters tall "
+puts "But if we want to convert his weight to Kilograms #{my_name} would
+weigh #{my_weight * lb_to_kg_conversion} Kg"
+puts "Also, he would be #{my_height * inch_to_cent_conversion} centimeters tall "
