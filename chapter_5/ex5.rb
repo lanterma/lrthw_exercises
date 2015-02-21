90c535e95f420fe6f0ae2ad1e9afdab96eb1ed1d
Matt Lanterman <Lanterma@gmail.com>
2015-02-20 17:59:12 -0700

    Added my_ to variables, to make the program run

--- chapter_5/ex5.rb
+++ chapter_5/ex5.rb
@@ -7,8 +7,8 @@ my_eyes = 'Brown'
 my_teeth = 'white'
 my_hair = 'Brown'
 
-lb_to_kg_conversion = weight * 0.45
-inch_to_cent_conversion = height * 2.5
+lb_to_kg_conversion = my_weight * 0.45
+inch_to_cent_conversion = my_height * 2.5
 
 puts "Let's talk about #{my_name}."
 puts "He's #{my_height} inches tall."
@@ -19,7 +19,7 @@ puts "His teeth are usually #{my_teeth} depending on the coffee."
 
 # I am going to try and get this next, trick line correct.
 puts "If I add #{my_age}, #{my_height}, and #{my_weight}
-I get #{age + height + weight}"
+I get #{my_age + my_height + my_weight}"
 
 # is this 'working out the math in Ruby?'
 puts "But if we want to convert his weight to Kilograms #{my_name} would
